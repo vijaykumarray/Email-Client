ceb36d97748607cf09afa35dd8216e4add5d0ec9
vijaykumarray <117904467+vijaykumarray@users.noreply.github.com>
2023-01-18 18:52:30 +0530

    Add files via upload

---
+++ Email-Client/src/main/java/com/dailycodesbuffer/springemailclient/SpringEmailClientApplication.java
@@ -0,0 +1,30 @@
+package com.dailycodesbuffer.springemailclient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import javax.mail.MessagingException;
+
+@SpringBootApplication
+public class SpringEmailClientApplication {
+
+	@Autowired
+	private EmailSenderService service;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmailClientApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void triggerMail() throws MessagingException {
+
+		service.sendEmailWithAttachment("vray18077@gmail.com",
+				"This is Email Body with Attachment...",
+				"This email has attachment",
+				"C:\\Users\\vijay kumar ray\\Downloads\\hacker.jpg");
+
+	}
+}
